503fdfa8e3b4d6c625485c9acf16f9d40155a59d
Young Master <caelestis.deimos@protonmail.com>
2023-04-17 19:59:38 -0400

    feat: :art: Added index files to manage imports/exports (cleaner)

---
+++ src/bot/commands/index.ts
@@ -0,0 +1,17 @@
+import addRemoveGroupCmd from "./addRemoveGroup.ts";
+import statsCmd from "./statsCmd.ts";
+import addRemoveUserCmd from "./addRemoveUser.ts";
+import randomCmd from "./randomCmd.ts";
+import settingsCmd from "./settingsCmd.ts";
+import fetchCmd from "./fetchCmd.ts";
+import infoCmd from "./infoCmd.ts";
+
+export {
+    addRemoveGroupCmd,
+    statsCmd,
+    addRemoveUserCmd,
+    randomCmd,
+    settingsCmd,
+    fetchCmd,
+    infoCmd,
+};
---
+++ src/bot/middlewares/index.ts
@@ -0,0 +1,9 @@
+import checkDailyUsageMiddleware from "./checkDailyUsage.ts";
+import protectCommandsMiddleware from "./protectCommands.ts";
+import jobQueueMiddleware from "./jobQueue.ts";
+
+export {
+    checkDailyUsageMiddleware,
+    protectCommandsMiddleware,
+    jobQueueMiddleware,
+};
