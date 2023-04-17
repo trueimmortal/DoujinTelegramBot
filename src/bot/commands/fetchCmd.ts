a388460cdf7f805cae66828aba985dbb5595a215
Young Master <caelestis.deimos@protonmail.com>
2023-04-17 19:54:47 -0400

    feat: :sparkles: Added /random and /fetch commands for doujins

---
+++ src/bot/commands/fetchCmd.ts
@@ -0,0 +1,63 @@
+import { fetchDoujin } from "../../backend/doujins.requests.ts";
+import {
+    updateDailyUsage,
+    getUser,
+} from "../../backend/users.requests.ts";
+import { LOG_LEVELS, ERROR_GIF } from "../../libs/constants.ts";
+import { Composer } from "../../libs/deps.ts";
+import {
+    NoResultsError,
+    NoExhentaiCookieError,
+    BadRequestError,
+} from "../../libs/errors.ts";
+import { BotContext } from "../../bot/types.ts";
+import { verifyUrl } from "../../libs/utils.ts";
+import { sendLoadingAnim, sendDoujin } from "../utils.ts";
+
+const composer = new Composer<BotContext>();
+
+/** Handle the /fetch command. */
+composer.command("fetch", async (ctx) => {
+    await getUser(ctx.from);
+    if (verifyUrl(ctx.match)) {
+        const loadingMessage = await sendLoadingAnim(ctx);
+        try {
+            await updateDailyUsage(ctx.from);
+            const doujin = await fetchDoujin(ctx.match);
+            await sendDoujin(ctx, doujin, loadingMessage);
+        } catch (error) {
+            console.error(LOG_LEVELS.ERROR, error.message);
+            if (error instanceof NoResultsError) {
+                await ctx.api.deleteMessage(
+                    loadingMessage.chat.id,
+                    loadingMessage.message_id
+                );
+                await ctx.replyWithAnimation(ERROR_GIF, {
+                    caption: "No results found at that URL!",
+                });
+            }
+            if (error instanceof NoExhentaiCookieError) {
+                await ctx.api.deleteMessage(
+                    loadingMessage.chat.id,
+                    loadingMessage.message_id
+                );
+                await ctx.replyWithAnimation(ERROR_GIF, {
+                    caption: error.caption,
+                });
+            }
+            if (error instanceof BadRequestError) {
+                await ctx.api.deleteMessage(
+                    loadingMessage.chat.id,
+                    loadingMessage.message_id
+                );
+                await ctx.replyWithAnimation(ERROR_GIF, {
+                    caption: "That url is not valid!",
+                });
+            }
+        }
+    } else {
+        await ctx.reply("Please pass a valid E-Hentai or ExHentai url!");
+    }
+});
+
+export default composer;
---
+++ src/bot/commands/randomCmd.ts
@@ -0,0 +1,58 @@
+import { getRandDoujin } from "../../backend/doujins.requests.ts";
+import { getUser, updateDailyUsage } from "../../backend/users.requests.ts";
+import { ERROR_GIF } from "../../libs/constants.ts";
+import { Composer, InputFile } from "../../libs/deps.ts";
+import {
+    InvalidTagFormatError,
+    NoDoujinFoundError,
+    NoExhentaiCookieError,
+} from "../../libs/errors.ts";
+import { BotContext } from "../../bot/types.ts";
+import { validateTags } from "../../libs/utils.ts";
+import { sendDoujin, sendLoadingAnim } from "../utils.ts";
+
+const composer = new Composer<BotContext>();
+
+// // /** Handle the /random command. */
+composer.command("random", async (ctx) => {
+    await getUser(ctx.from);
+    const loadingMessage = await sendLoadingAnim(ctx);
+    try {
+        if (!validateTags(ctx.match)) {
+            await ctx.api.deleteMessage(
+                loadingMessage.chat.id,
+                loadingMessage.message_id
+            );
+            await ctx.replyWithPhoto(new InputFile("./assets/taginfo.png"), {
+                caption:
+                    "Wrong tag format!, please refer to the image above for more info",
+            });
+        }
+        const doujin = await getRandDoujin(ctx.match);
+        await updateDailyUsage(ctx.from);
+        await sendDoujin(ctx, doujin, loadingMessage);
+    } catch (error) {
+        if (error instanceof InvalidTagFormatError) {
+        } else if (error instanceof NoDoujinFoundError) {
+            await ctx.api.deleteMessage(
+                loadingMessage.chat.id,
+                loadingMessage.message_id
+            );
+            await ctx.replyWithAnimation(ERROR_GIF, {
+                caption: `No results found with the tags ${ctx.match}`,
+            });
+        } else if (error instanceof NoExhentaiCookieError) {
+            await ctx.api.deleteMessage(
+                loadingMessage.chat.id,
+                loadingMessage.message_id
+            );
+            await ctx.replyWithAnimation(ERROR_GIF, {
+                caption: "Bad exhentai cookies!",
+            });
+        } else {
+            throw new Error(error.message);
+        }
+    }
+});
+
+export default composer;
