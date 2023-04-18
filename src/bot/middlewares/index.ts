15cb22669021ea0805177decc56ee1a3b92b8209
Young Master <caelestis.deimos@protonmail.com>
2023-04-17 20:42:18 -0400

    feat: :art: Added middleware import

--- src/bot/middlewares/index.ts
+++ src/bot/middlewares/index.ts
@@ -1,9 +1,11 @@
 import checkDailyUsageMiddleware from "./checkDailyUsage.ts";
 import protectCommandsMiddleware from "./protectCommands.ts";
 import jobQueueMiddleware from "./jobQueue.ts";
+import onlyUsersInGroup from "./onlyUsersInGroups.ts";
 
 export {
     checkDailyUsageMiddleware,
     protectCommandsMiddleware,
     jobQueueMiddleware,
+    onlyUsersInGroup,
 };
